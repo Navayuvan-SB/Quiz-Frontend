036d3a3383ccebf39d3f592c79840ae32254d5b1
Navayuvan <ns.navayuvan@gmail.com>
2021-02-05 22:32:21 +0530

    (feat)login: implemented login ui

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // Form field controls
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
-  ngOnInit(): void {
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+  ]);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // Login the user
+  loginUser() : void{
+    const cred = {
+      email: this.emailFormControl.value,
+      password: this.passwordFormControl.value
+    };
   }
 
 }
