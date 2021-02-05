fd5979d160302923ce3959cb41d9c91c0197a808
Navayuvan <ns.navayuvan@gmail.com>
2021-02-05 22:32:36 +0530

    (feat)signup: implemented signup ui

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  // Form field controls
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
-  constructor() { }
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+  ]);
 
-  ngOnInit(): void {
+  nameFormControl = new FormControl('', [Validators.required]);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // Create an account for the user
+  signUpUser() {
+    const cred = {
+      email: this.emailFormControl.value,
+      name: this.nameFormControl.value,
+      password: this.passwordFormControl.value,
+    };
   }
-
 }
