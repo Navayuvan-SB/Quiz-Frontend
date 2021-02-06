e7f0f058a0e0c62afd93c182444ec5b097eabd92
Navayuvan <ns.navayuvan@gmail.com>
2021-02-06 12:52:04 +0530

    (feat)routes: add route for admin quiz detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminQuizDetailsComponent } from './pages/admin-quiz-details/admin-quiz-details.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { QuizDetailComponent } from './pages/quiz-detail/quiz-detail.component';
@@ -8,13 +9,18 @@ import { ResultComponent } from './pages/result/result.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'login'},  
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: 'quiz', component: QuizComponent, pathMatch: 'full' },
   { path: 'quiz-detail', component: QuizDetailComponent, pathMatch: 'full' },
   { path: 'result', component: ResultComponent, pathMatch: 'full' },
+  {
+    path: 'admin-quiz-detail',
+    component: AdminQuizDetailsComponent,
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
