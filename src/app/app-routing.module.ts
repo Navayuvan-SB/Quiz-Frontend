c535745041b894295bf1f6a86a99130f4260e1a6
Navayuvan <ns.navayuvan@gmail.com>
2021-02-07 20:20:24 +0530

    (feat)result: result module completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,7 +45,7 @@ const routes: Routes = [
     canActivate: [UserGaurdGuard],
   },
   {
-    path: 'result',
+    path: 'result/:id',
     component: ResultComponent,
     pathMatch: 'full',
     canActivate: [UserGaurdGuard],
--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Result } from 'src/app/models/quiz.model';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-result',
@@ -7,11 +9,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./result.component.scss'],
 })
 export class ResultComponent implements OnInit {
-  constructor(private router: Router) {}
+  // Result data
+  // Result Members
+  result: Result = {
+    noOfCorrectAnswers: 0,
+    noOfTotalQuestions: 10,
+    noOfWrongAnswers: 0,
+    quizId: '',
+    timeTaken: '',
+    quizName: '',
+  };
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Fetch the result data
+    this.getResult();
+  }
 
-  navToHome() {
+  // Get Result data from localstorage if exist or hit a API
+  private async getResult(): Promise<void> {
+    this.route.paramMap.subscribe((params) => {
+      const id: string = params.get('id');
+
+      const localstorageData: Result = JSON.parse(localStorage.getItem(id));
+
+      if (localstorageData) {
+        this.result = localstorageData;
+      } else {
+        this.api.getAResult(id).subscribe((data) => {
+          this.result = data as Result;
+        });
+      }
+    });
+  }
+
+  navToHome(): void {
     this.router.navigate(['home']);
   }
 }
