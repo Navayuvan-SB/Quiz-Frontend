f2af507a3133988f5f593660b940e4e14982c8cd
Navayuvan <ns.navayuvan@gmail.com>
2021-02-07 17:19:46 +0530

    (add)modules: Material Components module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +13,7 @@ import { ResultComponent } from './pages/result/result.component';
 import { QuizDetailComponent } from './pages/quiz-detail/quiz-detail.component';
 import { AdminQuizDetailsComponent } from './pages/admin-quiz-details/admin-quiz-details.component';
 import { CreateQuizComponent } from './pages/create-quiz/create-quiz.component';
+import { PreloaderComponent } from './components/preloader/preloader.component';
 
 // Material Components Import
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -21,9 +22,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatSelectModule } from "@angular/material/select"
-import { MatSnackBarModule } from "@angular/material/snack-bar"
-import { MatIconModule } from "@angular/material/icon"
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LoaderInterceptor } from './interceptor/loader.interceptor';
 
 @NgModule({
   declarations: [
@@ -36,6 +39,7 @@ import { MatIconModule } from "@angular/material/icon"
     QuizDetailComponent,
     AdminQuizDetailsComponent,
     CreateQuizComponent,
+    PreloaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -53,9 +57,12 @@ import { MatIconModule } from "@angular/material/icon"
     MatDialogModule,
     MatSelectModule,
     MatSnackBarModule,
-    MatIconModule
+    MatIconModule,
+    MatProgressSpinnerModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
